7d42a00b1716a26aae0d021189276b2e3d7d4c7f
Hammad Abbas <hammadabbasoffc@gmail.com>
2024-06-02 20:14:31 +0500

    On branch main Your branch is up to date with 'origin/main'.

--- E-commerceApp/EcommerceBackend/src/middlewares/error.ts
+++ E-commerceApp/EcommerceBackend/src/middlewares/error.ts
@@ -1,8 +1,18 @@
-import { NextFunction, Request, Response } from "express"
+import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/utility-class.js";
 
-export const errorMiddleware = (err:Error,req:Request, res:Response, next:NextFunction)=>{
-    return res.status(400).json({
-        success: true,
-        message: "Some Error"
-    })
-}
+export const errorMiddleware = (
+  err: ErrorHandler,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+
+    err.message ||= "Internal Server Error";
+    err.statusCode ||= 500;
+
+  return res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
