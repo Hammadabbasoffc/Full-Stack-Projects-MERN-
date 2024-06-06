23f963b2eb9f4b09d33c646067a17892bde03741
Hammad Abbas <hammadabbasoffc@gmail.com>
2024-06-06 09:33:12 +0500

    make authintication for admin

--- E-commerceApp/EcommerceBackend/src/middlewares/auth.ts
+++ E-commerceApp/EcommerceBackend/src/middlewares/auth.ts
@@ -1,7 +1,18 @@
+import { User } from "../models/user.model.js";
+import ErrorHandler from "../utils/utility-class.js";
 import { TryCatch } from "./error.js";
 
+export const adminOnly = TryCatch(async (req, res, next) => {
+  const { id } = req.query;
 
+  if (!id) return next(new ErrorHandler("abhy login to kr", 401));
 
-export const adminOnly = TryCatch(async(req,res, next)=>{
-    
-})
+  const user = await User.findById(id);
+
+  if (!user) return next(new ErrorHandler("Oe fake hai tu", 401));
+
+  if (user.role !== "admin")
+    return next(new ErrorHandler("abhy admin ban admin", 401));
+
+  next();
+});
