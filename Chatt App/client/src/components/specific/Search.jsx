5ba19b374eb4a73b0f0b175c473d768c9961916a
Hammad Abbas <hammadabbasoffc@gmail.com>
2024-07-13 14:08:47 +0500

    making more components

--- Chatt App/client/src/components/specific/Search.jsx
+++ Chatt App/client/src/components/specific/Search.jsx
@@ -1,36 +1,59 @@
-import { useInputValidation } from '6pp'
-import { Dialog, DialogTitle, InputAdornment, Stack, TextField } from '@mui/material'
-import { Search as SearchIcon } from "@mui/icons-material";import React from 'react'
+import { useInputValidation } from "6pp";
+import { sampleUsers } from "../../constants/sampleData";
+import {
+  Dialog,
+  DialogTitle,
+  InputAdornment,
+  List,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { Search as SearchIcon } from "@mui/icons-material";
+import React, { useState } from "react";
+import UserItem from "../shared/UserItem";
 
 const Search = () => {
+  const [users, setUsers] = useState(sampleUsers);
+  const search = useInputValidation("");
 
-  const search = useInputValidation("")
-
+  let isLoadingSendFriendRequest = false;
+  const addFriendHandler = (id) => {
+    console.log(id);
+  };
 
   return (
     <Dialog open>
-        <Stack p={"2rem"} direction={"column"} width={"25rem"}>
-          <DialogTitle textAlign={"center"} >Find People</DialogTitle>
+      <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+        <DialogTitle textAlign={"center"}>Find People</DialogTitle>
 
-          <TextField 
-            label=""
-            value={search.value}
-            onChange={search.changeHandler}
-            variant='outlined'
-            size='small'
-            InputProps={{
-              startAdornment:(
-                <InputAdornment position='start'>
-                  <SearchIcon/>
-                </InputAdornment>
-              )
-            }}
-          
-          />
+        <TextField
+          label=""
+          value={search.value}
+          onChange={search.changeHandler}
+          variant="outlined"
+          size="small"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
 
-        </Stack>
+        <List>
+          {users.map((i) => {
+            return <UserItem
+              user={i}
+              key={i._id}
+              handler={addFriendHandler}
+              handlerIsLoading={isLoadingSendFriendRequest}
+            />;
+          })}
+        </List>
+      </Stack>
     </Dialog>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
