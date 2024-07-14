b874daeab2302566def3f1df95042d93b247fe48
Hammad Abbas <hammadabbasoffc@gmail.com>
2024-07-14 12:07:37 +0500

    making the chat component

--- Chatt App/client/src/components/Styles/StyledComponents.jsx
+++ Chatt App/client/src/components/Styles/StyledComponents.jsx
@@ -20,3 +20,13 @@ export const Link = styled(LinkComponent)`
     background-color: rgba(0, 0, 0, 0.1);
   }
 `;
+
+const InputBox = styled("input")`
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+  padding: 0 3rem;
+  border-radius: 1.5rem;
+  background-color: ${grayColor};
+`;
--- Chatt App/client/src/pages/Chat.jsx
+++ Chatt App/client/src/pages/Chat.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import {
+  AttachFile as AttachFileIcon,
+  Send as SendIcon,
+} from "@mui/icons-material";
+import { IconButton, Stack } from "@mui/material";
+import React, { Fragment } from "react";
+import FileMenu from "../components/dialogs/FileMenu";
+import AppLayout from "../components/layout/AppLayout";
+import { TypingLoader } from "../components/layout/Loaders";
+import MessageComponent from "../components/shared/MessageComponent";
+import { InputBox } from "../components/Styles/StyledComponents";
+import { grayColor, orange } from "../constants/color";
 
-const Chat = () => {
+const Chat = ({
+  chatId,
+  user,
+  containerRef,
+  bottomRef,
+  allMessages,
+  userTyping,
+  message,
+  messageOnChange,
+  submitHandler,
+  handleFileOpen,
+  fileMenuAnchor,
+}) => {
   return (
-    <div>Chat</div>
-  )
-}
+    <Fragment>
+      <Stack
+        ref={containerRef}
+        boxSizing={"border-box"}
+        padding={"1rem"}
+        spacing={"1rem"}
+        bgcolor={grayColor}
+        height={"90%"}
+        sx={{
+          overflowX: "hidden",
+          overflowY: "auto",
+        }}
+      >
+        {allMessages.map((i) => (
+          <MessageComponent key={i._id} message={i} user={user} />
+        ))}
 
-export default Chat
+        {userTyping && <TypingLoader />}
+
+        <div ref={bottomRef} />
+      </Stack>
+
+      <form
+        style={{
+          height: "10%",
+        }}
+        onSubmit={submitHandler}
+      >
+        <Stack
+          direction={"row"}
+          height={"100%"}
+          padding={"1rem"}
+          alignItems={"center"}
+          position={"relative"}
+        >
+          <IconButton
+            sx={{
+              position: "absolute",
+              left: "1.5rem",
+              rotate: "30deg",
+            }}
+            onClick={handleFileOpen}
+          >
+            <AttachFileIcon />
+          </IconButton>
+
+          <InputBox
+            placeholder="Type Message Here..."
+            value={message}
+            onChange={messageOnChange}
+          />
+
+          <IconButton
+            type="submit"
+            sx={{
+              rotate: "-30deg",
+              bgcolor: orange,
+              color: "white",
+              marginLeft: "1rem",
+              padding: "0.5rem",
+              "&:hover": {
+                bgcolor: "error.dark",
+              },
+            }}
+          >
+            <SendIcon />
+          </IconButton>
+        </Stack>
+      </form>
+
+      <FileMenu anchorE1={fileMenuAnchor} chatId={chatId} />
+    </Fragment>
+  );
+};
+
+export default AppLayout(Chat);
