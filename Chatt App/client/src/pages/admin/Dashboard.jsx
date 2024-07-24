77d0f903d71dc070d88d979a9be19c21182509c6
Hammad Abbas <hammadabbasoffc@gmail.com>
2024-07-24 21:46:55 +0500

    configuring chatt Admin app

---
+++ Chatt App/client/src/pages/admin/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AdminLayoout from '../../components/layout/AdminLayout'
+
+const Dashboard = () => {
+  return (
+
+    <AdminLayoout>
+         <div>Dashboard</div>
+    </AdminLayoout>
+
+   
+  )
+}
+
+export default Dashboard
